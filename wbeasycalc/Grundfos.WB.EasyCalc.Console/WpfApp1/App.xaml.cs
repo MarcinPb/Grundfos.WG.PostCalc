e5c1895be0b9e3bdce5f5c46014d0ec877b73a78
Marcin Przybylowicz <martin.przybylowicz@gmail.com>
2020-09-28 15:10:23 +0200

    Changed converters for float and percentage.

--- wbeasycalc/Grundfos.WB.EasyCalc.Console/WpfApp1/App.xaml.cs
+++ wbeasycalc/Grundfos.WB.EasyCalc.Console/WpfApp1/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace WpfApp1
 {
@@ -13,5 +16,13 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            CultureInfo info = new CultureInfo("pl-PL");
+            Thread.CurrentThread.CurrentCulture = info;
+            //Thread.CurrentThread.CurrentUICulture = info;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.Name)));
+        }
     }
 }
