61f490b3ff510c4838336525aa84a97130b0d560
Marcin Przybyłowicz <martin.przybylowicz@gmail.com>
2021-02-10 15:15:44 +0100

    Added ConfigMatrix class.

---
+++ WbEasyCalc/WbEasyCalc/DataModel/ConfigMatrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class ConfigMatrix
+    {
+        public int ConfigId { get; set; }
+        public int TypeId { get; set; }
+        public string CategoryName { get; set; }
+        public double? Ili { get; set; }
+        public double? Lcd10 { get; set; }
+        public double? Lcd20 { get; set; }
+        public double? Lcd30 { get; set; }
+        public double? Lcd40 { get; set; }
+        public double? Lcd50 { get; set; }
+    }
+}
